c5da0e360705879bffe54c49249803fd6e632042
Bram Droppers <bram.droppers@wur.nl>
2018-11-21 11:07:40 +0100

    update

--- vic/src/plugins/groundwater/gw_get_parameters.c
+++ vic/src/plugins/groundwater/gw_get_parameters.c
@@ -15,7 +15,7 @@ gw_get_global_parameters(char *cmdstr)
         sscanf(cmdstr, "%*s %s", flgstr);
         options.GROUNDWATER = str_to_bool(flgstr);
     }
-    if (strcasecmp("GROUNDWATER_EQUILIBRIUM", optstr) == 0) {
+    else if (strcasecmp("GROUNDWATER_EQUILIBRIUM", optstr) == 0) {
         sscanf(cmdstr, "%*s %s", flgstr);
         options.GW_EQUILIBRIUM = str_to_bool(flgstr);
     }
@@ -36,5 +36,8 @@ gw_validate_global_parameters(void)
 
     if (strcasecmp(filenames.groundwater.nc_filename, MISSING_S) == 0) {
         log_err("GROUNDWATER = TRUE but GROUNDWATER_PARAMETERS is missing");
+    }
+    if (options.GW_EQUILIBRIUM){
+        log_info("Running groundwater for equilibrium");
     }
 }
